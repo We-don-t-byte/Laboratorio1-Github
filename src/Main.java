3489c05d8968eabf4db861f8f61a0a2e199a807c
Santiago Sáenz <santiago.saenz@flip.org.co>
2022-05-11 23:25:15 -0500

    Adición de archivo Clase Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static int divideNumbers(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divideNumbers(10, 2));
+    }
+}
